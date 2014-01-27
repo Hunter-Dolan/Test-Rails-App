6a135f899e7ade5a0758330a9cc56a9a9a24d72b
Hunter Dolan <hunterhdolan@gmail.com>
2014-01-26 18:03:17 -0600

    Init

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 TestRailsApp::Application.routes.draw do
+  get "welcome/root"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#root'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
